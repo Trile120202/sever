9fb0b15d0a71f1cc3946f8d2640aaffc50f42ce5
Tri Le <trlminh120202@gmail.com>
2024-07-11 17:57:30 +0700

    api admin

--- controllers/productController.js
+++ controllers/productController.js
@@ -30,6 +30,27 @@ module.exports = {
             res.status(500).json(error)
         }
     },
+    updateProduct: async (req, res) => {
+        try {
+            const updatedProduct = await Product.findByIdAndUpdate(
+                req.params.id,
+                { $set: req.body },
+                { new: true }
+            );
+            res.status(200).json(updatedProduct);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+
+    deleteProduct: async (req, res) => {
+        try {
+            await Product.findByIdAndDelete(req.params.id);
+            res.status(200).json("Product deleted");
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
 
     searchProducts: async (req, res) => {
         try {
